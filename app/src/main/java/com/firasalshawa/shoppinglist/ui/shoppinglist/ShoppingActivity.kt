06a82d61eb687b49b0d9c2af9fa671d39fa81fe4
FIRAS AL SHAWA <f84155439@china.huawei.com>
2020-08-11 17:49:30 +0300

    apply kodin injection

--- app/src/main/java/com/firasalshawa/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/firasalshawa/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -12,15 +12,19 @@ import com.firasalshawa.shoppinglist.data.db.entities.ShoppingItem
 import com.firasalshawa.shoppinglist.data.repositories.ShoppingRepository
 import com.firasalshawa.shoppinglist.other.ShoppingItemAdapter
 import kotlinx.android.synthetic.main.activity_shopping.*
+import org.kodein.di.android.kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() ,KodeinAware{
+
+    override val kodein by kodein()
+    private val factory : ShoppingViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this,factory).get(ShoppingViewModel::class.java);
 
